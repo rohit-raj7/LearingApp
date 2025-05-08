4d0ad220a96bab2180d05a30dbf6d87988a69a7d
Rohit raj <126317686+rohit-raj7@users.noreply.github.com>
2025-05-08 22:04:05 +0530

    Update server.js

--- LMS/server/server.js
+++ LMS/server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js'
+import {clerkWebhooks} from './controllers/webhooks.js';
 
 // Insitilize Express
 
