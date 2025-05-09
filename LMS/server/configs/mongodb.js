0730e85b948e8ff6a3e2f5b03848094abc800b67
Rohit raj <126317686+rohit-raj7@users.noreply.github.com>
2025-05-09 09:23:34 +0530

    Update mongodb.js

--- LMS/server/configs/mongodb.js
+++ LMS/server/configs/mongodb.js
@@ -1,10 +1,26 @@
+// import mongoose from "mongoose";
+
+// // connect to the mongoDb database
+
+// const connectDB = async () => {
+//     mongoose.connection.on('connected', () => console.log("Database Connected"))
+
+//     await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+// }
+// export default connectDB;
+
+
+
 import mongoose from "mongoose";
 
-// connect to the mongoDb database
-
 const connectDB = async () => {
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+    console.log("Database Connected");
+  } catch (error) {
+    console.error("Database Connection Error:", error.message);
+    process.exit(1);
+  }
+};
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
-}
 export default connectDB;
